bf55d7159e359636571f1b70d08dd20a08e63365
Abraham Martin <amc203@cam.ac.uk>
2014-08-15 10:06:39 +0100

    initial code

---
+++ ucamibisclient/utils.py
@@ -0,0 +1,47 @@
+from django.core.exceptions import ValidationError
+from ibisclient import *
+
+conn = createConnection()
+
+def get_users_from_query(search_string):
+    """ Returns the list of people based on the search string using the lookup ucam service
+        :param search_string: the search string
+    """
+    persons = PersonMethods(conn).search(query=search_string)
+
+    return map((lambda person: {'crsid': person.identifier.value, 'visibleName': person.visibleName}),
+               persons)
+
+
+def return_visibleName_by_crsid(crsid):
+    person = PersonMethods(conn).getPerson(scheme='crsid', identifier=crsid)
+    return person.visibleName if person is not None else ''
+
+
+def get_groups_from_query(search_string):
+    """ Returns the list of groups based on the search string using the lookup ucam service
+        :param search_string: the search string
+    """
+    groups = GroupMethods(conn).search(query=search_string)
+
+    return map((lambda group: {'groupid': group.groupid, 'title': group.title}),
+               groups)
+
+
+def return_title_by_groupid(groupid):
+    group = GroupMethods(conn).getGroup(groupid=groupid)
+    # TODO If a group does not exists in lookup should we allow it?
+    if group is None:
+        raise ValidationError("The group with id %(groupid)s does not exist in Lookup", code='invalid',
+                              params={'groupid': groupid},)
+    return group.title if group is not None else ''
+
+
+def get_groups_of_a_user_in_lookup(user):
+    """ Returns the list of groups of a user in the lookup service
+    :param user: the user
+    :return: the list of group_ids
+    """
+
+    group_list = PersonMethods(conn).getGroups(scheme="crsid", identifier=user.username)
+    return map(lambda group: int(group.groupid), group_list)
---
+++ ucamibisclient/views.py
@@ -0,0 +1,17 @@
+import json
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from utils import get_users_from_query, get_groups_from_query
+
+
+@login_required
+def find_people(request):
+    persons = get_users_from_query(request.GET.get('query'))
+    return HttpResponse(json.dumps({'searchId_u': request.GET.get('searchId_u'), 'persons': persons}),
+                        content_type='application/json')
+
+@login_required
+def find_groups(request):
+    groups = get_groups_from_query(request.GET.get('query'))
+    return HttpResponse(json.dumps({'searchId_g': request.GET.get('searchId_g'), 'groups': groups}),
+                        content_type='application/json')
