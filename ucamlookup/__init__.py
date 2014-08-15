3dcd5bc8a1098a988f8cf187b425db7378a7466b
Abraham Martin <amc203@cam.ac.uk>
2014-08-15 17:15:07 +0100

    adds two receivers, for user and group
    The user receiver adds the visibleName value from lookup for this user
    to the last_name variable of the object of the class User.
    The group receiver assign the title of the group retrieved by lookup to
    the name variable of the object of the class Group

--- ucamlookup/__init__.py
+++ ucamlookup/__init__.py
@@ -1,1 +1,18 @@
+from django.contrib.auth.models import User, Group
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from utils import *
+
+
+@receiver(pre_save, sender=User)
+def add_name_to_user(instance):
+    user = instance
+    if user is not None:
+        user.last_name = return_visibleName_by_crsid(user.username)
+
+
+@receiver(pre_save, sender=Group)
+def add_title_to_group(instance):
+    group = instance
+    if group is not None:
+        group.name = return_title_by_groupid(group.id)
