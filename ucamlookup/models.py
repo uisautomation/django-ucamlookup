3be9943fda74a5b629d9beed0dceb10e9c56b109
Abraham Martin <amc203@cam.ac.uk>
2014-09-10 11:34:25 +0100

    Update model desc for python3 compatibility

--- ucamlookup/models.py
+++ ucamlookup/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class LookupGroup(models.Model):
     name = models.CharField(max_length=255)
     lookup_id = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.lookup_id)
