59211672e54bbe3a66eb43dba650061e4a2ff4be
Abraham Martin <amc203@cam.ac.uk>
2015-05-13 16:39:44 +0100

    Limitation of the maximum characters the User.last_name (30) and Group.name (80) can have

--- ucamlookup/__init__.py
+++ ucamlookup/__init__.py
@@ -8,11 +8,11 @@ from utils import *
 def add_name_to_user(instance, **kwargs):
     user = instance
     if user is not None:
-        user.last_name = return_visibleName_by_crsid(user.username)
+        user.last_name = return_visibleName_by_crsid(user.username)[:30]
 
 
 @receiver(pre_save, sender=LookupGroup)
 def add_title_to_group(instance, **kwargs):
     group = instance
     if group is not None:
-        group.name = return_title_by_groupid(group.lookup_id)
+        group.name = return_title_by_groupid(group.lookup_id)[:80]
